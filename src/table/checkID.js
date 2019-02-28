4afa7dcaf8b56b93442f7c1984f3c849f36194a7
loori-r <loor@list.ru>
2019-02-28 13:52:54 +0300

    feat: write user name in orders table

---
+++ src/table/checkID.js
@@ -0,0 +1,12 @@
+import Users from '../users/users';
+
+const checkID = (info_users, order) => {
+  info_users.map((item_user) => {
+    if (item_user.id === order.user_id) {
+      const user = new Users(item_user)
+      order.userInfo(user)
+    }
+  })
+}
+
+export default checkID
