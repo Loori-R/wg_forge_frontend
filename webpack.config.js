09db69cad08024cdd3bf6d4445131ad39fb0a7b9
loori-r <loor@list.ru>
2019-02-28 16:25:34 +0300

    feat: add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,14 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
@@ -17,7 +25,7 @@ module.exports = {
     contentBase: path.join(__dirname, "dist"),
     open: true,
     port: 9000,
-    before: function(app, server) {
+    before: function (app, server) {
       app.use("/api", express.static(path.join(__dirname, "data")));
     }
   }
