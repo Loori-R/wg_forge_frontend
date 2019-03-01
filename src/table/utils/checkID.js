a22441dc642f7d016cac425e5149e6f0ed682842
loori-r <loor@list.ru>
2019-03-01 16:50:32 +0300

    refact: use lodash

--- src/table/utils/checkID.js
+++ src/table/utils/checkID.js
@@ -1,7 +1,7 @@
 import Users from '../../users/users';
 
 const checkID = (info_users, order) => {
-  info_users.map((item_user) => {
+  _.map(info_users, (item_user) => {
     if (item_user.id === order.user_id) {
       const user = new Users(item_user)
       order.userInfo(user)
